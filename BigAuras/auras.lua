4fa1f7d6b2ba068c3095fa93926c5dc6b1df3f12
Qblolz <frostQB@mail.ru>
2022-04-10 19:19:44 +0300

    Update auras.lua

--- BigAuras/auras.lua
+++ BigAuras/auras.lua
@@ -334,9 +334,13 @@ BigAurasSpells.spells = {
 
     --other
         [308876] = {category = "other", priority = 1}, -- Necrotic trinket(sirus.su)
+            [308874] = {parent = 308876},
+            [308875] = {parent = 308876},
+            [308876] = {parent = 308876},
             [308877] = {parent = 308876},
             [308878] = {parent = 308876},
             [308879] = {parent = 308876},
+            [316612] = {parent = 308876},
         
         
     --roots
